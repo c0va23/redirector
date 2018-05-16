823c45d0a9d4f7f4cd78e6a0ba18aaac2f01fb83
Dmitrij Fedorenko <c0va23@gmail.com>
2018-05-16 22:51:26 +0300

    Fix store mock

--- test/mocks/storemock.go
+++ test/mocks/storemock.go
@@ -18,9 +18,15 @@ func (s *StoreMock) ListHostRules() ([]models.HostRules, error) {
 	return args.Get(0).([]models.HostRules), args.Error(1)
 }
 
-// ReplaceHostRules for store mock
-func (s *StoreMock) ReplaceHostRules(hostRules models.HostRules) error {
-	args := s.MethodCalled("ReplaceHostRules", hostRules)
+// CreateHostRules for store mock
+func (s *StoreMock) CreateHostRules(hostRules models.HostRules) error {
+	args := s.MethodCalled("CreateHostRules", hostRules)
+	return args.Error(0)
+}
+
+// UpdateHostRules for store mock
+func (s *StoreMock) UpdateHostRules(host string, hostRules models.HostRules) error {
+	args := s.MethodCalled("UpdateHostRules", host, hostRules)
 	return args.Error(0)
 }
 
